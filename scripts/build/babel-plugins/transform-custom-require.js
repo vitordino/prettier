8482a5273e5118daca67c3029cfca2d9c6e197ed
fisker <lionkay@gmail.com>
2020-02-05 00:56:38 +0800

    Simplify `require` transform plugin

--- scripts/build/babel-plugins/transform-custom-require.js
+++ scripts/build/babel-plugins/transform-custom-require.js
@@ -1,55 +1,22 @@
 "use strict";
 
-//
-// BEFORE:
-//   eval("require")("./path/to/file")
-//   eval("require")(identifier)
-//   eval("require").cache
-//
-// AFTER:
-//   require("./file")
-//   require(identifier)
-//   require.cache
-//
+// `eval("require")` -> `require`
 
 module.exports = function(babel) {
   const t = babel.types;
+  const isEvalRequire = node =>
+    t.isCallExpression(node) &&
+    t.isIdentifier(node.callee, { name: "eval" }) &&
+    node.arguments.length === 1 &&
+    t.isLiteral(node.arguments[0], { value: "require" });
 
   return {
     visitor: {
       CallExpression(path) {
-        const { node } = path;
-        if (isEvalRequire(node.callee) && node.arguments.length === 1) {
-          let arg = node.arguments[0];
-          if (t.isLiteral(arg) && arg.value.startsWith(".")) {
-            const value = "." + arg.value.slice(arg.value.lastIndexOf("/"));
-            arg = t.stringLiteral(value);
-          }
-          path.replaceWith(t.callExpression(t.identifier("require"), [arg]));
-        }
-      },
-      MemberExpression(path) {
-        const { node } = path;
-        if (isEvalRequire(node.object)) {
-          path.replaceWith(
-            t.memberExpression(
-              t.identifier("require"),
-              node.property,
-              node.compute,
-              node.optional
-            )
-          );
+        if (isEvalRequire(path.node)) {
+          path.replaceWith(t.identifier("require"));
         }
       }
     }
   };
-
-  function isEvalRequire(node) {
-    return (
-      t.isCallExpression(node) &&
-      t.isIdentifier(node.callee, { name: "eval" }) &&
-      node.arguments.length === 1 &&
-      t.isLiteral(node.arguments[0], { value: "require" })
-    );
-  }
 };
